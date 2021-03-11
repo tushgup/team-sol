e4c4d3db15cf210b83e65d8cc6a5e1ccf80ad962
Tushar Gupta <tush1998@gmail.com>
2021-03-11 18:08:14 -0500

    popup editable enabled for existing marker layers

---
+++ js/popupMod.js
@@ -0,0 +1,191 @@
+// Adding nametag labels to all popup-able leaflet layers
+const sourceTypes = ['Layer','Circle','CircleMarker','Marker','Polyline','Polygon','ImageOverlay','VideoOverlay','SVGOverlay','Rectangle','LayerGroup','FeatureGroup','GeoJSON']
+
+sourceTypes.forEach( type => {
+   L[type].include({
+      nametag: type.toLowerCase()
+   })
+})
+
+//  Adding new options to the default options of a popup
+L.Popup.mergeOptions({
+   removable: false,
+   editable: false,
+})
+
+// Modifying the popup mechanics
+L.Popup.include({
+
+   // modifying the _initLayout method to include edit and remove buttons, if those options are enabled
+
+   //  ----------------    Source code  ---------------------------- //
+   // original from https://github.com/Leaflet/Leaflet/blob/master/src/layer/Popup.js
+   _initLayout: function () {
+      var prefix = 'leaflet-popup',
+          container = this._container = L.DomUtil.create('div',
+         prefix + ' ' + (this.options.className || '') +
+         ' leaflet-zoom-animated');
+
+      var wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
+      this._contentNode = L.DomUtil.create('div', prefix + '-content', wrapper);
+
+      L.DomEvent.disableClickPropagation(wrapper);
+      L.DomEvent.disableScrollPropagation(this._contentNode);
+      L.DomEvent.on(wrapper, 'contextmenu', L.DomEvent.stopPropagation);
+
+      this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
+      this._tip = L.DomUtil.create('div', prefix + '-tip', this._tipContainer);
+
+      if (this.options.closeButton) {
+         var closeButton = this._closeButton = L.DomUtil.create('a', prefix + '-close-button', container);
+         closeButton.href = '#close';
+         closeButton.innerHTML = '&#215;';
+
+         L.DomEvent.on(closeButton, 'click', this._onCloseButtonClick, this);
+      }
+
+      //  ----------------    Source code  ---------------------------- //
+
+
+      //  ---------------    My additions  --------------------------- //
+
+      var nametag
+
+      if (this.options.nametag){
+         nametag = this.options.nametag
+      } else if (this._source) {
+         nametag =  this._source.nametag
+      } else {
+         nametag = "popup"
+      }
+
+      if (this.options.removable && !this.options.editable){
+         var userActionButtons = this._userActionButtons = L.DomUtil.create('div', prefix + '-useraction-buttons', wrapper);
+         var removeButton = this._removeButton = L.DomUtil.create('a', prefix + '-remove-button', userActionButtons);
+         removeButton.href = '#close';
+         removeButton.innerHTML = `Remove this ${nametag}`;
+         this.options.minWidth = 110;
+
+         L.DomEvent.on(removeButton, 'click', this._onRemoveButtonClick, this);
+      }
+
+      if (this.options.editable && !this.options.removable){
+         var userActionButtons = this._userActionButtons = L.DomUtil.create('div', prefix + '-useraction-buttons', wrapper);
+         var editButton = this._editButton = L.DomUtil.create('a', prefix + '-edit-button', userActionButtons);
+         editButton.href = '#edit';
+         editButton.innerHTML = 'Edit';
+
+         L.DomEvent.on(editButton, 'click', this._onEditButtonClick, this);
+      }
+
+      if (this.options.editable && this.options.removable){
+         var userActionButtons = this._userActionButtons = L.DomUtil.create('div', prefix + '-useraction-buttons', wrapper);
+         var removeButton = this._removeButton = L.DomUtil.create('a', prefix + '-remove-button', userActionButtons);
+         removeButton.href = '#close';
+         removeButton.innerHTML = `Remove this ${nametag}`;
+         var editButton = this._editButton = L.DomUtil.create('a', prefix + '-edit-button', userActionButtons);
+         editButton.href = '#edit';
+         editButton.innerHTML = 'Edit';
+         this.options.minWidth = 160;
+
+         L.DomEvent.on(removeButton, 'click', this._onRemoveButtonClick, this);
+         L.DomEvent.on(editButton, 'click', this._onEditButtonClick, this);
+      }
+   },
+
+   _onRemoveButtonClick: function (e) {
+      this._source.remove();
+      L.DomEvent.stop(e);
+      var event = new CustomEvent("removeMarker", {
+        detail: {
+          marker: this._source
+        }
+      });
+      document.dispatchEvent(event);
+   },
+
+   _onEditButtonClick: function (e) {
+      //Needs to be defined first to capture width before changes are applied
+      var inputFieldWidth = this._inputFieldWidth = this._container.offsetWidth - 2*19;
+
+      this._contentNode.style.display = "none";
+      this._userActionButtons.style.display = "none";
+
+      var wrapper = this._wrapper;
+      var editScreen = this._editScreen = L.DomUtil.create('div', 'leaflet-popup-edit-screen', wrapper)
+      var inputField = this._inputField = L.DomUtil.create('div', 'leaflet-popup-input', editScreen);
+      inputField.setAttribute("contenteditable", "true");
+      inputField.innerHTML = this.getContent()
+
+
+      //  -----------  Making the input field grow till max width ------- //
+      inputField.style.width = inputFieldWidth + 'px';
+      var inputFieldDiv = L.DomUtil.get(this._inputField);
+
+      // create invisible div to measure the text width in pixels
+      var ruler = L.DomUtil.create('div', 'leaflet-popup-input-ruler', editScreen);
+
+      let thisStandIn = this;
+
+      // Padd event listener to the textinput to trigger a check
+      this._inputField.addEventListener("keydown", function(){
+      // Check to see if the popup is already at its maxWidth
+      // and that text doesnt take up whole field
+         if (thisStandIn._container.offsetWidth < thisStandIn.options.maxWidth + 38
+            && thisStandIn._inputFieldWidth + 5 < inputFieldDiv.clientWidth){
+            ruler.innerHTML = inputField.innerHTML;
+
+            if (ruler.offsetWidth + 20 > inputFieldDiv.clientWidth){
+               console.log('expand now');
+               inputField.style.width = thisStandIn._inputField.style.width = ruler.offsetWidth + 10 + 'px';
+               thisStandIn.update();
+            }
+         }
+      }, false)
+
+
+      var inputActions = this._inputActions = L.DomUtil.create('div', 'leaflet-popup-input-actions', editScreen);
+      var cancelButton = this._cancelButton = L.DomUtil.create('a', 'leaflet-popup-input-cancel', inputActions);
+      cancelButton.href = '#cancel';
+      cancelButton.innerHTML = 'Cancel';
+      var saveButton = this._saveButton = L.DomUtil.create('a', 'leaflet-popup-input-save', inputActions);
+      saveButton.href = "#save";
+      saveButton.innerHTML = 'Save';
+
+      L.DomEvent.on(cancelButton, 'click', this._onCancelButtonClick, this)
+      L.DomEvent.on(saveButton, 'click', this._onSaveButtonClick, this)
+
+      this.update();
+      L.DomEvent.stop(e);
+   },
+
+
+   _onCancelButtonClick: function (e) {
+      L.DomUtil.remove(this._editScreen);
+      this._contentNode.style.display = "block";
+      this._userActionButtons.style.display = "flex";
+
+      this.update();
+      L.DomEvent.stop(e);
+   },
+
+   _onSaveButtonClick: function (e) {
+      var inputField = this._inputField;
+      if (inputField.innerHTML.length > 0){
+         this.setContent(inputField.innerHTML)
+      } else {
+         alert('Enter something');
+      };
+
+      L.DomUtil.remove(this._editScreen);
+      this._contentNode.style.display = "block";
+      this._userActionButtons.style.display = "flex";
+
+      this.update();
+      L.DomEvent.stop(e);
+
+      //  ---------------------End my additions --------------------------------------- //
+
+
+   }
+})
